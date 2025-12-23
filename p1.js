ea2ecfde9e0dc6f1e04327f56768afca552b2c7f
Tanisha-Verma-16 <tanisha.verma.070121@gmail.com>
2025-12-24 01:47:27 +0530

    p1 update

--- p1.js
+++ p1.js
@@ -3,3 +3,23 @@ const age = 45;
 var occupation = "Actor";
 
 console.table({ name, age, occupation });
+
+let greeting = `Hello, my name is ${name}. I am ${age} years old and I work as an ${occupation}.`;
+console.log(greeting);
+
+let movieList = ["28 Days Later", "Inception", "Dunkirk"];
+
+let something;
+console.log(something); // undefined
+
+let isActive = true;
+console.log(`Is ${name} active? ${isActive}`);
+
+function getBirthYear(currentYear, age) {
+    return currentYear - age;
+}
+let birthYear = getBirthYear(2024, age);
+console.log(`${name} was born in ${birthYear}.`);
+
+let otherThing = null; // NULL IS EMPTY NOT UNDEFINED
+console.log(`The value of otherThing is: ${otherThing}`);
